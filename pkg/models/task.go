97b219fcbcad91f5da028ab28dd635113a53fe0f
425devon <425devon@gmail.com>
2018-12-28 14:32:37 -0800

    corrected json id field

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type Task struct {
-	ID        bson.ObjectId `bson:"_id" json:"id"`
+	ID        bson.ObjectId `bson:"_id" json:"_id"`
 	Name      string        `bson:"name" json:"name"`
 	Completed bool          `bson:"completed" json:"completed"`
 }
