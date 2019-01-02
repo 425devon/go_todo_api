0bda7e19f1fe72001bc5226e4512117a66a0af7e
425devon <425devon@gmail.com>
2019-01-02 10:42:00 -0800

    fixed typo

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type Task struct {
-	ID        bson.ObjectId `bson:"_id" json:"_id"`
+	ID        bson.ObjectId `bson:"_id" json:"id"`
 	Name      string        `bson:"name" json:"name"`
 	Completed bool          `bson:"completed" json:"completed"`
 }
